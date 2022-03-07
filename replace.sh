37779136825149aa753986e864751e40ce010772
Kqpa <epicmanborax@gmail.com>
2022-03-07 09:05:36 +0300

    changes

--- replace.sh
+++ replace.sh
@@ -9,15 +9,14 @@ printf "Cursor path is set to '$CURSOR_DIRECTORY'. Is this correct? [y/n]: " && 
 if [ "$option" != "${option#[Yyes]}" ]; then
 	echo "Continuing..."
 	cd $CURSOR_DIRECTORY
-else
+elif [ "$option" != "${option#[Nno]}" ]; then
 	printf "Enter the FULL path of the cursor location: " && read -r NEW_CURSOR_DIRECTORY
-	
 	if [[ ! -d "$NEW_CURSOR_DIRECTORY" || -z "$NEW_CURSOR_DIRECTORY" ]] ;then 
 		echo "Directory does not exist." && exit
 	else
 		cd $NEW_CURSOR_DIRECTORY
 	fi
-fi
+else echo "Invalid option." && exit; fi
 
 # Removing the old cursors
 
@@ -25,8 +24,14 @@ rm ArrowCursor.png ArrowFarCursor.png IBeamCursor.png
 
 # Downloading the old cursors
 
-wget 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/ArrowCursor.png' &&
-wget 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/ArrowFarCursor.png' && 
-wget 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/IBeamCursor.png'
+wget -q --show-progress 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/ArrowCursor.png' &&
+wget -q --show-progress 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/ArrowFarCursor.png' && 
+wget -q --show-progress 'https://raw.githubusercontent.com/Kqpa/old-roblox-cursors/master/assets/old/IBeamCursor.png'
 
-echo "Finished replacing. Restart ROBLOX if it's already running."
+# Summary message
+
+if [ "$option" != "${option#[Yyes]}" ]; then
+	echo "Finished replacing on '$CURSOR_DIRECTORY'. Restart ROBLOX if it's already running."
+elif [ "$option" != "${option#[Nno]}" ]; then
+	echo "Finished replacing on '$NEW_CURSOR_DIRECTORY'. Restart ROBLOX if it's already running."
+fi
